296ba38db5122dd0ddc5023bf2c7eebbcfc2fa7f
Tackyou <e970501e@opayq.com>
2016-03-18 14:34:18 +0100

    Optimized winning chance script

--- gleamio.user.js
+++ gleamio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gleam.io Autosolve
 // @namespace    GLEAM
-// @version      1.1
+// @version      1.2
 // @description  lets save some time
 // @author       Tackyou
 // @license      https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/LICENSE
@@ -13,23 +13,17 @@
 // @grant        none
 // ==/UserScript==
 
-if (document.getElementById("current-entries") !== null) {
-    var own = parseInt(document.querySelector(".status.ng-binding").innerHTML);
-    var total = parseInt(document.querySelector(".current.ng-binding").innerHTML);
-    var chance = Math.round(10000 * own / total ) / 100;
-
-    $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"> <span class="square-describe mont"> <span class="status small"> <span class="current ng-binding" id="winning-chance">NaN</span> </span> <span class="description ng-binding">Winning Chance</span> </span> </div>');
-
-    var elems = document.querySelectorAll("div.square-row.row-fluid.center.ng-scope > .span4");
-
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].setAttribute("style", "width:25%;")
-    }
-
-    document.getElementById("winning-chance").innerHTML = chance + "%";
-}
-
 console.log('[GLEAM] Welcome!');
+if ($("#current-entries") !== null) {
+    $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
+    $("div.square-row.row-fluid.center.ng-scope > .span4").width('25%');
+    setTimeout(function(){ 
+        var own = parseInt($(".status.ng-binding").text());
+        var total = parseInt($(".current.ng-binding").text());
+        var chance = Math.round(10000 * own / total) / 100;
+        $("#winning-chance").text(chance+"%");
+    }, 250);
+}
 var gi = 0;
 var gleam = setInterval(function(){
         if($("[ng-click*='entry_method']").length>0){
