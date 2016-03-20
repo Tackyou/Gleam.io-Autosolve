93d7dc1bc5c8e75090c3e7fa36b11776e4c0292b
Tackyou <e970501e@opayq.com>
2016-03-20 20:15:25 +0100

    Reworked the whole script, more stable now

--- gleamio.user.js
+++ gleamio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gleam.io Autosolve
 // @namespace    GLEAM
-// @version      1.3
+// @version      1.4
 // @description  lets save some time
 // @author       Tackyou
 // @license      https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/LICENSE
@@ -13,11 +13,68 @@
 // @grant        none
 // ==/UserScript==
 
+var wo = window.open;
+window.open = function(){
+   return;
+}
+var processed = [];
 console.log('[GLEAM] Welcome!');
-if ($("#current-entries") !== null) {
-    $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
-    $("div.square-row.row-fluid.center.ng-scope > .span4").width('25%');
-    setTimeout(setChance, 250);
+$(function(){
+    gleam();
+    if ($("#current-entries") !== null) {
+        $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
+        $("div.square-row.row-fluid.center.ng-scope > .span4").width('25%');
+        setTimeout(setChance, 400);
+    }
+});
+function gleam(){
+    if($("div.redeem-container").is(':visible')){
+        console.log('[GLEAM] already completed');
+        $('div.incentive-description h3.ng-binding.ng-scope').append('<div style="background:gold;color:#000;border-radius:10px;">Already completed!</div>');
+    }else{
+        var y = 0, x = setInterval(function(){
+            y++;
+            var g = 0;
+            $('.entry-method').each(function(index){
+                g++;
+                var elem = $(this);
+                if(!elem.hasClass('completed-entry-method') && processed.indexOf(g)==-1){
+                    var type = $('span.icon-wrapper i', elem);
+                    var text = '#'+g+' '+$.trim($('.text .ng-binding.ng-scope', elem).text());
+                    console.log("[GLEAM] Processing: "+text);
+                    if(type.hasClass('fa-heart')){
+                        checkStatus(elem);
+                    }else{
+                        $('.entry_details a.btn', elem).trigger('click');
+                        $('.tally', elem).trigger('click');
+                        setTimeout(function(){setChance();checkStatus(elem);},400);
+                    }
+                    processed.push(g);
+                }
+            });
+            if(y == 20){
+                clearInterval(x);
+                setTimeout(function(){setChance();},1500);
+                window.open = wo;
+            }
+        }, 1000);
+    }
+}
+function checkStatus(elem){
+    var i = 0;
+    var v = setInterval(function(){
+        i++;
+        var tally = $("[ng-class*='tallyIcon']", elem);
+        if(!tally.hasClass('fa-spin') || i == 100){
+            if(tally.hasClass('fa-chevron-down')){
+                $('.tally', elem).trigger('click');
+            }
+            if(!tally.hasClass('fa-check') && !tally.hasClass('fa-clock-o')){
+                $('.tally', elem).css('background', '#F2C32E');
+            }
+            clearInterval(v);
+        }
+    }, 100);
 }
 function setChance(){
     var own = parseInt($(".status.ng-binding").text());
@@ -25,24 +82,3 @@ function setChance(){
     var chance = Math.round(10000 * own / total) / 100;
     $("#winning-chance").text(chance+"%");
 }
-var gi = 0;
-var gleam = setInterval(function(){
-        if($("[ng-click*='entry_method']").length>0){
-            if($("div.redeem-container").length<1 || $("div.redeem-container").is(':hidden')){
-                $('.entry_details .visit-trigger a.btn').trigger('click');
-                $("[ng-click*='entry_method']").trigger('click');
-                $("[ng-class*='tallyIcon']").trigger('click');
-                console.log('[GLEAM] Action performed');
-                setChance();
-            }else{
-                console.log('[GLEAM] already completed');
-                $('div.incentive-description h3.ng-binding.ng-scope').append('<div style="background:gold;color:#000;border-radius:10px;">Already completed!</div>');
-                clearInterval(gleam);
-            }
-        }   
-        gi++;
-        if(gi>6){
-            clearInterval(gleam);
-            setChance();
-        }
-}, 500);
