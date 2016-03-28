140eb822c2d9f4c02d148abf8bf8f0e22fe8529a
Tackyou <e970501e@opayq.com>
2016-03-28 22:37:57 +0200

    Version 1.7 optimized script and fixed some bugs

--- gleamio.user.js
+++ gleamio.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Gleam.io Autosolve
 // @namespace    GLEAM
-// @version      1.6
+// @version      1.7
 // @description  lets save some time
 // @author       Tackyou
 // @license      https://raw.githubusercontent.com/Tackyou/Gleam.io-Autosolve/master/LICENSE
@@ -17,10 +17,11 @@ var wo = window.open;
 function non(){
    return;
 }
-var processed = [];
-var ytreplace = [];
+var ytstatus = 0;
+var elements = 0;
 console.log('[GLEAM] Welcome!');
 $(window).load(function(){
+    elements = $('.entry-method').length;
     gleam();
     if ($("#current-entries") != undefined) {
         $('.span4.blue-square.ng-scope').after('<div class="span4 green-square ng-scope"><span class="square-describe mont"><span class="status small"><span class="current ng-binding" id="winning-chance">NaN</span></span><span class="description ng-binding">Winning Chance</span></span></div>');
@@ -33,57 +34,75 @@ function gleam(){
         console.log('[GLEAM] already completed');
         $('div.incentive-description h3.ng-binding.ng-scope').append('<div style="background:gold;color:#000;border-radius:10px;">Already completed!</div>');
     }else{
-        var g = 0;
-        $('.entry-method').each(function(){
-            // check if tooltip says "have to solve other steps first" to prevent trying hidden tasks
-            g++;
-            var elem = $(this);
-            elem.data('$scope').entry_method.mandatory = true;
-            if(!elem.hasClass('completed-entry-method') && elem.is(':visible') && processed.indexOf(g)==-1){
-                window.open = non;
-                var type = $('span.icon-wrapper i', elem);
-                var text = '#'+g+' '+$.trim($('.text .ng-binding.ng-scope', elem).text());
-                console.log("[GLEAM] Processing: "+text);
-                if(type.hasClass('fa-heart')){
-                    checkStatus(elem, g);
+        solveStep(0);
+    }
+}
+function solveStep(m){
+    if(m > elements){
+        checkStatus();
+        return;
+    }
+    var elem = $('.entry-method:eq('+m+')');
+    var scop = elem.data('$scope');
+    if(scop != null){ scop.entry_method.mandatory = true; }
+    if(!elem.hasClass('completed-entry-method') && elem.is(':visible')){
+        window.open = non;
+        var type = $('span.icon-wrapper i', elem);
+        var text = '#'+m+' '+$.trim($('.text .ng-binding.ng-scope', elem).text());
+        console.log("[GLEAM] Processing: "+text);
+        if(!type.hasClass('fa-heart')){
+            if(type.hasClass('fa-youtube')){
+                var ytskip = false;
+                var yt = $('iframe.youtube', elem);
+                if(ytstatus == 0){
+                    $('.tally', elem).trigger('click');
+                    ytstatus = 1;
                 }else{
-                    if(type.hasClass('fa-youtube')){
-                        var yt = $('iframe.youtube', elem);
+                    if(ytstatus == 1){
                         if(yt.attr('src') != undefined){
-                            if(ytreplace.indexOf(g)==-1){
-                                var yl = yt.attr('src').replace('autoplay=0', 'autoplay=1');
-                                yt.attr('src', yl+'&start=99999999999');
-                                ytreplace.push(g);
-                            }
+                            var yl = yt.attr('src').replace('autoplay=0', 'autoplay=1');
+                            yt.attr('src', yl+'&start=99999999999');
+                            ytstatus = 2;
+                        }else{
+                            ytskip = true;
+                            ytstatus = 0;
+                            m++;
+                        }
+                    }else{
+                        if(ytstatus == 2){
                             var b = $('button.btn[ng-click]', elem);
                             if(b.is(':visible')){
                                 b.trigger('click');
-                                checkStatus(elem, g);
+                                m++;
+                                ytstatus = 0;
                             }
-                            return true;
                         }
                     }
-                    $('.btn', elem).trigger('click');
-                    $('.tally', elem).trigger('click');
-                    checkStatus(elem, g);
                 }
-                window.open = wo;
+                setTimeout(function(){solveStep(m);}, 100);
+                if(!ytskip){ return; }
             }
-        });
-        setChance();
-    }
-}
-function checkStatus(elem, g){
-    var tally = $('[ng-class*="tallyIcon"]', elem);
-    if(!tally.hasClass('fa-spin')){
-        if(tally.hasClass('fa-chevron-down')){
+            $('.btn', elem).trigger('click');
             $('.tally', elem).trigger('click');
         }
-        if(!tally.hasClass('fa-check') && !tally.hasClass('fa-clock-o')){
-            $('.tally', elem).css('background', '#F2C32E');
-        }
-        processed.push(g);
+        window.open = wo;
     }
+    m++;
+    solveStep(m);
+}
+function checkStatus(){
+    $('.entry-method').each(function(){
+        var elem = $(this);
+        var tally = $('[ng-class*="tallyIcon"]', elem);
+        if(!tally.hasClass('fa-spin')){
+            if(tally.hasClass('fa-chevron-down')){
+                $('.tally', elem).trigger('click');
+            }
+            if(!tally.hasClass('fa-check') && !tally.hasClass('fa-clock-o')){
+                $('.tally', elem).css('background', '#F2C32E');
+            }
+        }
+    });
     setChance();
 }
 function setChance(){
